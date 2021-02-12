9f7c278d699909c087b38d198b3688e1b302760f
elyrb <74579750+elyrb@users.noreply.github.com>
2021-02-13 00:19:21 +0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -9,7 +9,14 @@ new Vue({
       currentTime: null,
       isTimerPlaying: false,
       tracks: [
-        
+        {
+          name: "secr3t mess4ge",
+          artist: "brayle",
+          cover: "https://raw.github.com/elyrb/fy/main/img/01.jpg",
+          source: "https://raw.github.com/elyrb/fy/main/mp3/pjgdb.m4a",
+          url: " ",
+          favorited: false
+        },
         {
           name: "Umaga",
           artist: "The Juans",
